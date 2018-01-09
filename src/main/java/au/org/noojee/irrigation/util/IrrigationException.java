815b615015b8356f658568eb2ef2dd1c5e3173c9
Brett Sutton <bsutton@noojee.com.au>
2018-01-09 19:04:50 +1100

    refactor.

---
+++ src/main/java/au/org/noojee/irrigation/util/IrrigationException.java
@@ -0,0 +1,12 @@
+package au.org.noojee.irrigation.util;
+
+public class IrrigationException extends Error {
+
+	public IrrigationException(Exception e)
+	{
+		super(e);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
