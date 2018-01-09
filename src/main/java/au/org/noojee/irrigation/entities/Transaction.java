54a8775e2d499a108368e634a5e134ee6231c9f7
Brett Sutton <bsutton@noojee.com.au>
2018-01-09 19:03:55 +1100

    changed transaction to require an EM.

---
+++ src/main/java/au/org/noojee/irrigation/entities/Transaction.java
@@ -0,0 +1,37 @@
+package au.org.noojee.irrigation.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Transaction implements AutoCloseable
+{
+	Logger logger = LogManager.getLogger();
+	private EntityManager em;
+	private EntityTransaction transaction;
+
+	public Transaction(EntityManager em)
+	{
+		transaction = em.getTransaction();
+		transaction.begin();
+	}
+
+	public void commit()
+	{
+		transaction.commit();
+	}
+	
+	@Override
+	public void close()
+	{
+		if (transaction.isActive())
+		{
+			logger.error("Rolling back uncommitted transaction.");
+			transaction.rollback();
+		}
+		
+	}
+
+}
