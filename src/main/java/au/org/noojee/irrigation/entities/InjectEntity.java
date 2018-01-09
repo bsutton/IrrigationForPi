274c71cf902da20e91673379479e58c93e6ff5f6
Brett Sutton <bsutton@noojee.com.au>
2018-01-09 19:03:33 +1100

    experimented with aspect 4J.
    still not using it.

---
+++ src/main/java/au/org/noojee/irrigation/entities/InjectEntity.java
@@ -0,0 +1,17 @@
+package au.org.noojee.irrigation.entities;
+
+
+/**
+ * injects an EntityManager into the thread (if it doesn't already exists) and 
+ * starts a transaction.
+ * 
+ * When your method returns the transaction will be committed.
+ * If a transaction already exists you will be joined to the existing transaction.
+ * 
+ * @author bsutton
+ *
+ */
+public @interface InjectEntity
+{
+
+}
