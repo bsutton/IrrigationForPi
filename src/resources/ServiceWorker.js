178132d8acc03b0085f750d5c713dd497b8d4078
Brett Sutton <bsutton@noojee.com.au>
2018-01-24 14:28:53 +1100

    changed logging to info
    formatted code.

--- src/resources/ServiceWorker.js
+++ src/resources/ServiceWorker.js
@@ -2,8 +2,8 @@ var CACHE_NAME = 'serviceworker-ex';
 var CACHE_VERSION = 8;
 
 var filesToCache = [
-//  '/',
-//  '/index.html',
+// '/',
+// '/index.html',
   // '/css/styles.css',
   '/irrigation/js/app.js',
   '/irrigation/VAADIN/themes/mytheme/styles.css?v=8.1.5',
@@ -83,75 +83,90 @@ var filesToCache = [
   '/irrigation/VAADIN/themes/valo/fonts/open-sans/OpenSans-Regular-webfont.woff'
 ];
 
-self.oninstall = function(event) {
+self.oninstall = function(event) 
+{
+	console.info("Service worker: oninstall called");
 	
-	console.warn("Service worker: oninstall called");
-	
-  event.waitUntil(
-    caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+	event.waitUntil(
+			caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(function(cache) 
+					{
+						return cache.addAll(filesToCache);
+					})
+	);
 };
 
-self.onactivate = function(event) {
-	
-  console.warn("Service worker: onactive called");
-  var currentCacheName = CACHE_NAME + '-v' + CACHE_VERSION;
-  caches.keys().then(function(cacheNames) {
-    return Promise.all(
-      cacheNames.map(function(cacheName) {
-        if (cacheName.indexOf(CACHE_NAME) == -1) {
-          return;
-        }
+self.onactivate = function(event) 
+{
+	console.info("Service worker: onactive called");
+  
+	var currentCacheName = CACHE_NAME + '-v' + CACHE_VERSION;
+  
+	caches.keys().then(function(cacheNames) 
+			{
+				return Promise.all(
+						cacheNames.map(function(cacheName) 
+						{
+							if (cacheName.indexOf(CACHE_NAME) == -1) 
+							{
+								return;
+							}
 
-        if (cacheName != currentCacheName) {
-          return caches.delete(cacheName);
-        }
-      })
-    );
-  });
+					        if (cacheName != currentCacheName) 
+					        {
+					        	return caches.delete(cacheName);
+					        }
+						})
+				);
+			});
 };
 
-self.onfetch = function(event) {
-
-  var request = event.request;
+self.onfetch = function(event) 
+{
+	var request = event.request;
+	console.debug("Service worker: onfetch called" + request.url);
   
-  console.warn("Service worker: onfetch called" + request.url);
-  
-  event.respondWith(
-    caches.match(request).then(function(response) {
-      if (response) {
-        return response;
-      }
-
-      return fetch(request).then(function(response) {
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(
-          function(cache) {
-            cache.put(request, responseToCache).catch(function(err) {
-              console.warn(request.url + ': ' + err.message);
-            });
-          });
-        return response;
-      });
-    })
-  );
+	event.respondWith(
+			caches.match(request).then(function(response) 
+					{
+						if (response) 
+						{
+							return response;
+						}
+						return fetch(request).then(function(response) 
+								{
+									var responseToCache = response.clone();
+									caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(
+											function(cache) 
+											{
+												cache.put(request, responseToCache)
+												.catch(function(err) 
+												{
+													console.info("Unable to cache:" + request.url + ': ' + err.message);
+												});
+											});
+									return response;
+								});
+					})
+	);
 };
 
 
 // Communicate via MessageChannel.
-self.addEventListener('message', function(event) {
-  console.log(`Received message from main thread: ${event.data}`);
-  event.ports[0].postMessage(`Got message! Sending direct message back - "${event.data}"`);
-});
+self.addEventListener('message', function(event) 
+		{
+			console.info(`Received message from main thread: ${event.data}`);
+			event.ports[0].postMessage(`Got message! Sending direct message back - "${event.data}"`);
+		});
 
 // Broadcast via postMessage.
-function sendMessage(message) {
-  self.clients.matchAll().then(function(clients) {
-    clients.map(function(client) {
-      return client.postMessage(message);
-    })
-  });
+function sendMessage(message) 
+{
+	self.clients.matchAll().then(function(clients) 
+			{
+				clients.map(function(client) 
+						{
+							return client.postMessage(message);
+						})
+			});
 }
 
