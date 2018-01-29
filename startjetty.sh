615a608e63d05f90d33aa1213df2cf5f98b014f6
Brett Sutton <bsutton@noojee.com.au>
2018-01-30 09:43:48 +1100

    cert bot experiment.

--- startjetty.sh
+++
@@ -1,9 +0,0 @@
- 
- export PI4J_PLATFORM=Simulated
- export SimulatedPlatform="RaspberryPi GPIO Provider"
-
- # mvn jetty:run
-
- #mvn exec:java -Dexec.mainClass="au.org.noojee.irrigation.JettyServer"
-
-java -cp target/irrigation-1.0-SNAPSHOT-classes.jar au.org.noojee.irrigation.JettyServer
