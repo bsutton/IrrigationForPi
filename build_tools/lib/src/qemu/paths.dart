c38c79c1a791371a1444e2b4da81d7354a638179
Brett Sutton <bsutton@noojee.com.au>
2023-01-09 21:06:44 +1100

    fixed path problems on windows

--- build_tools/lib/src/qemu/paths.dart
+++ build_tools/lib/src/qemu/paths.dart
@@ -4,4 +4,4 @@ String get qemuSystemArm => 'qemu-system-arm';
 
 String? get qemuSystemArmPath => join(vmQemuPath, qemuSystemArm);
 
-String get vmQemuPath => 'C:Program Filesqemu';
+String get vmQemuPath => r'C:\Program Files\qemu';
